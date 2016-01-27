ee38f688fceb3cb2987345ea996207efe3ffb870
James Muraca <jmuraca@gmail.com>
2016-01-27 22:15:51 +1100

    added scaling to SVG object viewbox added accordion menu

--- static/js/cutter.js
+++ static/js/cutter.js
@@ -1,7 +1,6 @@
 window.onload = function() 
 {	
 	var file_input = document.getElementById('file_input');
-	
 	file_input.addEventListener('change', function(e) 
 	{
 				
@@ -18,29 +17,29 @@ window.onload = function()
 			success: function(data) 
 			{
 				d = new Date();
+				$('#image_preview object').attr("data", "svg/pattern.svg?"+d.getTime());
+			},
+		});
+	});
+
+	$('#upload-file-btn').click(function() {
+		var form_data = new FormData($('#upload-file')[0]);
+		$.ajax({
+			type: 'POST',
+			url: '/uploadajax',
+			data: form_data,
+			contentType: false,
+			cache: false,
+			processData: false,
+			async: false,
+			success: function(data) 
+			{
+				d = new Date();
 				$('#image_preview img').attr("src", "svg/pattern.svg?"+d.getTime());
 			},
 		});
 	});
-	
-    $('#upload-file-btn').click(function() {
-        var form_data = new FormData($('#upload-file')[0]);
-        $.ajax({
-            type: 'POST',
-            url: '/uploadajax',
-            data: form_data,
-            contentType: false,
-            cache: false,
-            processData: false,
-            async: false,
-            success: function(data) 
-			{
-				d = new Date();
-                $('#image_preview img').attr("src", "svg/pattern.svg?"+d.getTime());
-            },
-        });
-    });
-	
+
 	$('.command').click(function() 
 	{
 		var url = this.href;
@@ -57,5 +56,26 @@ window.onload = function()
 		return false; // prevent default
 
 		console.log(attributes);
-    });
+	});
+	
+	var object = document.getElementsByTagName('object')[0];
+	object.addEventListener('load', function(){
+		scale_svg();
+	});
+
+	scale_svg();
+}
+
+function scale_svg()
+{
+	var svg = document.getElementsByTagName('object')[0].contentDocument.getElementsByTagName('svg')[0];
+	var bbox = svg.getBBox();
+	var viewBox = [bbox.x, bbox.y, bbox.width, bbox.height].join(" ");
+	svg.setAttribute("viewBox", viewBox);	
+	svg.viewBox = viewBox;
+	
+	svg.setAttribute("height", "100%");
+	svg.setAttribute("width", "100%");
+	svg.height = "100%";
+	svg.height = "100%";
 }
